e08d0e7c7eb12ec1fc552f8f9b638f68d5669865
valdanite <vicvaldezhidalgo@gmail.com>
2022-04-10 20:28:10 -0700

    Initial commit

---
+++ navBarButtons.jsx
@@ -0,0 +1,52 @@
+// Ex 3 - write out all items with their stock number
+// provide a button and use onClick={moveToCart} to move 1 item into the Shopping Cart
+// use React.useState to keep track of items in the Cart.
+// use React.useState to keep track of Stock items
+// list out the Cart items in another column
+function NavBar({ stockitems }) {
+  const [cart, setCart] = React.useState([]);
+  const [stock, setStock] = React.useState(stockitems);
+  const { Button } = ReactBootstrap;
+  // event apple:2
+  const moveToCart = e => {
+    let [name, num] = e.target.innerHTML.split(":"); // innerHTML should be format name:3
+    // use newStock = stock.map to find "name" and decrease number in stock by 1
+    // only if instock is >=  do we move item to Cart and update stock
+    let newStock = stock.map((item, index) => {
+      item.name == name && item.instock > 0 && item.instock-- && setCart([...cart, name]);
+      return item;
+    });
+    setStock(newStock);
+  };
+  const updatedCart = cart.map((item, index) => {
+    return (
+      <Button key={index}>{item}</Button>
+    )
+  });
+  const updatedList = stock.map((item, index) => {
+    return (
+      <Button onClick={moveToCart} key={index}>
+        {item.name}:{item.instock}
+      </Button>
+    );
+  });
+  // note that React needs to have a single Parent
+  return (
+    <>
+      <ul style={{ listStyleType: "none" }}>{updatedList}</ul>
+      <h1>Shopping Cart</h1>
+      <ul style={{ listStyleType: "none"}}>{updatedCart}</ul>
+    </>
+  );
+}
+const menuItems = [
+  { name: "apple", instock: 2 },
+  { name: "pineapple", instock: 3 },
+  { name: "pear", instock: 0 },
+  { name: "peach", instock: 3 },
+  { name: "orange", instock: 1 }
+];
+ReactDOM.render(
+  <NavBar stockitems={menuItems} />,
+  document.getElementById("root")
+);
